1baa031d34dbe5252e61eb735f4602f1dbc153c8
Kevin Morrison <thrashjazzassassin@gmail.com>
2017-07-13 21:44:09 +0100

    init upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var rot = 0;
+var zoom = 1.0;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    img = loadImage("sky-night-stars.jpg");
+}
+
+function draw() {
+  zoom -= 0.0001;
+    translate(width / 2, height / 2);
+    rotate(rot);
+    rot += 0.001;
+    image(img, -width * zoom, -height * zoom, width * 2 * zoom, height * 2 * zoom);
+
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
