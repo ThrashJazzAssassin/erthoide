64a559a66c23b63ac0f1ac8b9aec21dc3b198dc2
Kevin Morrison <thrashjazzassassin@gmail.com>
2018-01-30 21:09:55 +0000

    fix zoom

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var rot = 0;
 var zoom = 1.0;
+let zoomdir = 1;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -7,12 +8,14 @@ function setup() {
 }
 
 function draw() {
-  zoom -= 0.0001;
+    if (zoom < 1 || zoom >1.5) {
+        zoomdir *= -1;
+    }
+    zoom -= 0.0002*zoomdir;
     translate(width / 2, height / 2);
     rotate(rot);
     rot += 0.001;
     image(img, -width * zoom, -height * zoom, width * 2 * zoom, height * 2 * zoom);
-
 }
 
 function windowResized() {
